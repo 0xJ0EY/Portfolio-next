a0348aa71b4b368217adaebbc60a113c37622f47
Joey de Ruiter <contact@joeyderuiter.me>
2023-04-12 16:00:52 +0200

    Basic renderer construction / rendering / destroying

--- apps/web/components/renderer/Renderer.tsx
+++ apps/web/components/renderer/Renderer.tsx
@@ -1,49 +1,101 @@
 import styles from './Renderer.module.css'
 import { RefObject, useEffect, useRef } from "react";
-import { BoxGeometry, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, WebGLRenderer } from "three";
+import { BoxGeometry, Camera, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, WebGLRenderer } from "three";
 import { calculateAspectRatio } from './util';
+import { CSS3DObject, CSS3DRenderer } from "three/examples/jsm/renderers/CSS3DRenderer";
+
+const createRenderScenes = (): [Scene, Scene, Scene] => {
+  return [new Scene(), new Scene(), new Scene()];
+}
+
+const createCamera = (fov: number, aspectRatio: number): PerspectiveCamera => {
+  const camera = new PerspectiveCamera(fov, aspectRatio, 0.1, 1000);
+
+  camera.position.z = 5;
+
+  return camera;
+}
+
+const resizeCamera = (camera: PerspectiveCamera, aspectRatio: number): void => {
+  camera.aspect = aspectRatio;
+  camera.updateProjectionMatrix();
+}
+
+const createRenderers = (width: number, height: number): [WebGLRenderer, CSS3DRenderer] => {
+  const webglRenderer = new WebGLRenderer();
+  const cssRenderer = new CSS3DRenderer();
+
+  webglRenderer.setSize(width, height);
+  cssRenderer.setSize(width, height);
+
+  return [webglRenderer,  cssRenderer];
+}
+
+const resizeRenderers = (webGlRenderer: WebGLRenderer, cssRenderer: CSS3DRenderer, width: number, height: number): void => {
+  webGlRenderer.setSize(width, height);
+  cssRenderer.setSize(width, height);
+};
 
 export const Renderer = () => {
   const mountRef: RefObject<HTMLDivElement> = useRef(null);
+  const cssOutputRef: RefObject<HTMLDivElement> = useRef(null);
+  const webglOutputRef: RefObject<HTMLDivElement> = useRef(null);
 
   useEffect(() => {
     const domNode = mountRef.current;
+    const cssRendererNode = cssOutputRef.current;
+    const webglRenderNode = webglOutputRef.current;
+
     if (!domNode) { return; }
 
-    const scene = new Scene();
-    const camera = new PerspectiveCamera(75, calculateAspectRatio(domNode), 0.1, 1000);
-    const renderer = new WebGLRenderer();
+    let animationFrameId: number | null = null;
 
-    renderer.setSize(domNode.clientWidth, domNode.clientHeight);
+    const [scene, cutoutScene, cssScene] = createRenderScenes();
+    const camera = createCamera(75, calculateAspectRatio(domNode));
+    const [renderer, cssRenderer] = createRenderers(domNode.clientWidth, domNode.clientHeight);
 
-    mountRef.current?.appendChild(renderer.domElement);
+    cssRendererNode?.appendChild(cssRenderer.domElement);
+    webglRenderNode?.appendChild(renderer.domElement);
 
     const geo = new BoxGeometry(1, 1, 1);
     const mat = new MeshBasicMaterial({ color: 0x00FF00 });
     const mesh = new Mesh(geo, mat);
 
     scene.add(mesh);
-    camera.position.z = 5;
 
     const animate = function() {
-      requestAnimationFrame(animate);
+      animationFrameId = requestAnimationFrame(animate);
 
       mesh.rotation.x += 0.01;
       mesh.rotation.y += 0.01;
 
       renderer.render(scene, camera);
     }
-
+    
     const onWindowResize = function() {
-      camera.aspect = calculateAspectRatio(domNode);
-      camera.updateProjectionMatrix();
-      renderer.setSize(domNode.clientWidth, domNode.clientHeight);
+      resizeCamera(camera, calculateAspectRatio(domNode));
+      resizeRenderers(renderer, cssRenderer, domNode.clientWidth, domNode.clientHeight);
+    }
+
+    const onDestroy = function() {
+      if (animationFrameId) { cancelAnimationFrame(animationFrameId); }
+
+      renderer.dispose();
+      renderer.forceContextLoss();
+
+      cssRendererNode?.removeChild(cssRenderer.domElement);
+      webglRenderNode?.removeChild(renderer.domElement);
     }
 
     window.addEventListener('resize', onWindowResize, false);
     animate();
 
-    return () => { domNode?.removeChild(renderer.domElement) };
+    return () => onDestroy();
   }, []);
-  return <div className={styles.renderer} ref={mountRef}></div>;
+  return (
+    <div className={styles.renderer} ref={mountRef}>
+      <div className={styles.cssOutput} ref={cssOutputRef}></div>
+      <div className={styles.webglOutput} ref={webglOutputRef}></div>
+    </div>
+  );
 };
