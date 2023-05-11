f24f82f8feb6b6f890c4e694a80ce86a85a44c98
Joey de Ruiter <contact@joeyderuiter.me>
2023-05-11 11:53:33 +0200

    Actually clean up the event listeners

--- apps/web/components/renderer/camera/MouseInputHandler.ts
+++ apps/web/components/renderer/camera/MouseInputHandler.ts
@@ -1,33 +1,48 @@
 import { CameraHandler, CameraHandlerState, PointerData } from "./CameraHandlers";
 
 export class MouseInputHandler {
+  private onMouseDownListener: (evt: MouseEvent) => void;
+  private onMouseUpListener: (evt: MouseEvent) => void;
+  private onMouseMoveListener: (evt: MouseEvent) => void;
+  private onContextMenuListener: (evt: MouseEvent) => void;
+  private onWheelListener: (evt: WheelEvent) => void;
+  private onMouseLeaveListener: (evt: MouseEvent) => void;
+
   constructor(
     private handler: CameraHandler,
     private cssRenderNode: HTMLElement,
     private webglRenderNode: HTMLElement
   ) {
+
+    this.onMouseDownListener    = this.onMouseDown.bind(this);
+    this.onMouseUpListener      = this.onMouseUp.bind(this);
+    this.onMouseMoveListener    = this.onMouseMove.bind(this);
+    this.onContextMenuListener  = this.onContextMenu.bind(this);
+    this.onWheelListener        = this.onWheel.bind(this);
+    this.onMouseLeaveListener   = this.onMouseLeave.bind(this);
+
+    window.addEventListener('mousedown', this.onMouseDownListener);
+    window.addEventListener('mouseup', this.onMouseUpListener);
+    window.addEventListener('mousemove', this.onMouseMoveListener);
+    window.addEventListener('contextmenu', this.onContextMenuListener);
+    window.addEventListener('wheel', this.onWheelListener);
+    window.addEventListener('mouseleave', this.onMouseLeaveListener);
+    
     this.create();
   }
 
   create(): void {
     this.cssRenderNode.style.touchAction    = 'none';
     this.webglRenderNode.style.touchAction  = 'none';
-
-    window.addEventListener('mousedown', this.onMouseDown.bind(this));
-    window.addEventListener('mouseup', this.onMouseUp.bind(this));
-    window.addEventListener('mousemove', this.onMouseMove.bind(this));
-    window.addEventListener('contextmenu', this.onContextMenu.bind(this));
-    window.addEventListener('wheel', this.onWheel.bind(this));
-    window.addEventListener('mouseleave', this.onMouseLeave.bind(this));
   }
 
   destroy(): void {
-    window.removeEventListener('mousedown', this.onMouseDown.bind(this));
-    window.removeEventListener('mouseup', this.onMouseUp.bind(this));
-    window.removeEventListener('mousemove', this.onMouseMove.bind(this));
-    window.removeEventListener('contextmenu', this.onContextMenu.bind(this));
-    window.removeEventListener('wheel', this.onWheel.bind(this));
-    window.removeEventListener('mouseleave', this.onMouseLeave.bind(this));
+    window.removeEventListener('mousedown', this.onMouseDownListener);
+    window.removeEventListener('mouseup', this.onMouseUpListener);
+    window.removeEventListener('mousemove', this.onMouseMoveListener);
+    window.removeEventListener('contextmenu', this.onContextMenuListener);
+    window.removeEventListener('wheel', this.onWheelListener);
+    window.removeEventListener('mouseleave', this.onMouseLeaveListener);
   }
 
   private onMouseDown(evt: MouseEvent) {
@@ -36,6 +51,7 @@ export class MouseInputHandler {
   }
 
   private onMouseUp(evt: MouseEvent) {
+    
     const data = PointerData.fromMouseEvent(evt);
     this.handler.onPointerUp(data);
   }
