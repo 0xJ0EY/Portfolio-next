de4037a621da64579fce3488ebea8a5feadd90cd
Joey de Ruiter <contact@joeyderuiter.me>
2023-12-12 15:23:25 +0100

    Added a base for the observer

--- apps/desktop/apis/Sound/Sound.ts
+++ apps/desktop/apis/Sound/Sound.ts
@@ -1,7 +1,23 @@
-export class SoundService {
+import { ObserverSubject } from "@/data/Observer";
+
+export class SoundService extends ObserverSubject<boolean> {
   private enabled = false;
 
   public isEnabled(): boolean {
     return this.enabled;
+  }
+
+  public enable(): void {
+    this.enabled = true;
+    this.notifyEnabledStatus();
+  }
+
+  public disable(): void {
+    this.enabled = false;
+    this.notifyEnabledStatus();
+  }
+
+  private notifyEnabledStatus(): void {
+    this.notify(this.enabled);
   }
 }
