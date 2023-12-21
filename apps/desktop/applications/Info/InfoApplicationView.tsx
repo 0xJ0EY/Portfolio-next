0e13c49897b05bc77891b3254838fb543147958a
Joey de Ruiter <contact@joeyderuiter.me>
2023-12-21 18:40:55 +0100

    Some small info app changes

--- apps/desktop/applications/Info/InfoApplicationView.tsx
+++ apps/desktop/applications/Info/InfoApplicationView.tsx
@@ -1,11 +1,22 @@
 import { WindowProps } from '@/components/WindowManagement/WindowCompositor';
 import { useEffect, useState } from 'react';
 
+function getTargetUrl(time: number): string {
+  const env = process.env.NEXT_PUBLIC_VERCEL_ENV ?? 'local';
+
+  if (env === 'local') {
+    return `http://192.168.178.134:8080/waifu?t=${time}`;
+  } else {
+    return `https://waifu.joeyderuiter.me/waifu?t=${time}`;
+  }
+}
+
 export default function InfoApplicationView(props: WindowProps) {
   const { application, windowContext } = props;
 
   const [time, _] = useState(Date.now());
-
+  const url = getTargetUrl(time);
+  
   function onClickButton() {
     application.apis.sound.play('/sounds/meow.mp3', 0.25);
   }
@@ -21,7 +32,7 @@ export default function InfoApplicationView(props: WindowProps) {
         width: '100%',
         height: '100%',
         display: 'block',
-        background: `url(http://192.168.178.134:8080/waifu?t=${time})`,
+        background: `url(${url})`,
         backgroundSize: 'contain',
         backgroundPosition: 'center',
         backgroundRepeat: 'no-repeat'
