f39db8c847c4848c262675366720c4ebf1cb354b
Joey de Ruiter <contact@joeyderuiter.me>
2024-01-26 11:33:49 +0100

    Actually use the keyboards sounds

--- apps/desktop/components/PeripheralSounds/PeripheralSounds.tsx
+++ apps/desktop/components/PeripheralSounds/PeripheralSounds.tsx
@@ -4,8 +4,16 @@ import { SystemAPIs } from "../OperatingSystem";
 export function PeripheralSounds(props: { apis: SystemAPIs }) {
   const soundService = props.apis.sound;
 
-  function onKeyboardPress() {
-    soundService.play('/sounds/meow.mp3', 0.5 * Math.random());
+  function onKeyboardPress(evt: KeyboardEvent) {
+    const audioFragments = [
+      '/sounds/keyboard1.mp3',
+      '/sounds/keyboard2.mp3',
+      '/sounds/keyboard3.mp3',
+    ];
+
+    const randomAudioFragment = audioFragments[Math.floor(Math.random() * audioFragments.length)];
+
+    soundService.play(randomAudioFragment, 1);
   }
 
   useEffect(() => {
