c171373c2963cdf23663cb60baa5e9c497c31177
Joey de Ruiter <contact@joeyderuiter.me>
2024-01-30 16:52:52 +0100

    Added some code to support the keyboard aswell

--- apps/desktop/components/PeripheralSounds/PeripheralSounds.tsx
+++ apps/desktop/components/PeripheralSounds/PeripheralSounds.tsx
@@ -11,6 +11,14 @@ const PointerSecondaryKey = "pointer_secondary";
 
 const PrimaryMouseButton = 0x00;
 
+const SpaceBarKeyAudioFragments: AudioFragment[] = [
+  { onDown: '/sounds/meow.mp3', onUp: '/sounds/meow.mp3' },
+];
+
+const RegularKeyAudioFragments: AudioFragment[] = [
+  { onDown: '/sounds/meow.mp3', onUp: '/sounds/meow.mp3' },
+];
+
 const LeftMouseButtonAudioFragments: AudioFragment[] = [
   { onDown: '/sounds/left_mouse_down_1.mp3', onUp: '/sounds/left_mouse_up_1.mp3' },
   { onDown: '/sounds/left_mouse_down_2.mp3', onUp: '/sounds/left_mouse_up_2.mp3' },
@@ -27,9 +35,31 @@ function chooseRandomAudioFragment(fragments: AudioFragment[]): AudioFragment {
   return fragments[Math.floor(Math.random() * fragments.length)];
 }
 
+function chooseRandomKeyboardAudioFragment(code: string): AudioFragment {
+  switch (code) {
+    case "Space": return chooseRandomAudioFragment(SpaceBarKeyAudioFragments);
+    default: return chooseRandomAudioFragment(RegularKeyAudioFragments);
+  }
+}
+
 export function PeripheralSounds(props: { apis: SystemAPIs }) {
   const soundService = props.apis.sound;
   const activeSounds = useRef<Record<string, AudioFragment>>({});
+
+  function onKeyDown(evt: KeyboardEvent) {
+    const code = evt.code;
+    activeSounds.current[code] = chooseRandomKeyboardAudioFragment(code);
+
+    const audioFragment = activeSounds.current[code].onDown;
+    if (audioFragment) { soundService.play(audioFragment, 0.6); }
+  }
+
+  function onKeyUp(evt: KeyboardEvent) {
+    const code = evt.code;
+
+    const audioFragment = activeSounds.current[code].onDown;
+    if (audioFragment) { soundService.play(audioFragment, 0.6); }
+  }
 
   function onPointerDown(evt: PointerEvent) {
     const key = evt.button === PrimaryMouseButton ? PointerPrimaryKey : PointerSecondaryKey;
@@ -50,10 +80,16 @@ export function PeripheralSounds(props: { apis: SystemAPIs }) {
   }
 
   useEffect(() => {
+    window.addEventListener('keydown', onKeyDown);
+    window.addEventListener('keyup', onKeyUp);
+
     window.addEventListener('pointerdown', onPointerDown);
     window.addEventListener('pointerup', onPointerUp);
 
     return () => {
+      window.removeEventListener('keydown', onKeyDown);
+      window.removeEventListener('keyup', onKeyUp);
+      
       window.removeEventListener('pointerdown', onPointerDown);
       window.removeEventListener('pointerup', onPointerUp);
     }
@@ -61,105 +97,3 @@ export function PeripheralSounds(props: { apis: SystemAPIs }) {
 
   return <></>
 }
-
-/*
-export function PeripheralSounds(props: { apis: SystemAPIs }) {
-  const soundService = props.apis.sound;
-
-  function onBackspaceOrEnterKeyboardPress(): string {
-    const audioFragments = [
-      '/sounds/keyboard_long_key_1.mp3',
-      '/sounds/keyboard_long_key_2.mp3',
-    ];
-
-    return audioFragments[Math.floor(Math.random() * audioFragments.length)];
-  }
-
-  function onSpaceBarKeyboardPress(): string {
-    const audioFragments = [
-      '/sounds/keyboard_space_bar_1.mp3',
-      '/sounds/keyboard_space_bar_2.mp3',
-      '/sounds/keyboard_space_bar_3.mp3',
-    ];
-
-    return audioFragments[Math.floor(Math.random() * audioFragments.length)];
-  }
-
-  function onNormalKeyboardPress(): string {
-    const audioFragments = [
-      '/sounds/keyboard_1.mp3',
-      '/sounds/keyboard_2.mp3',
-      '/sounds/keyboard_3.mp3',
-    ];
-
-    return audioFragments[Math.floor(Math.random() * audioFragments.length)];
-  }
-
-  function onKeyboardPress(evt: KeyboardEvent) {
-    let audioFragment: string | null = null;
-
-    switch (evt.code) {
-      case "Enter":
-      case "Backspace":
-      case "ShiftLeft":
-      case "ShiftRight":
-        audioFragment = onBackspaceOrEnterKeyboardPress();
-        break;
-      case "Space":
-        audioFragment = onSpaceBarKeyboardPress();
-        break;
-      default:
-        audioFragment = onNormalKeyboardPress();
-    }
-
-    soundService.play(audioFragment, 0.6);
-  }
-
-  function playLeftMouseClickSound() {
-    const audioFragments = [
-      '/sounds/left_mouse_1.mp3',
-      '/sounds/left_mouse_2.mp3',
-      '/sounds/left_mouse_3.mp3',
-    ];
-
-    const audioFragment = audioFragments[Math.floor(Math.random() * audioFragments.length)];
-    soundService.play(audioFragment, 1);
-  }
-
-  function playRightMouseClickSound() {
-    const audioFragments = [
-      '/sounds/right_mouse_1.mp3',
-      '/sounds/right_mouse_2.mp3',
-    ];
-
-    const audioFragment = audioFragments[Math.floor(Math.random() * audioFragments.length)];
-    soundService.play(audioFragment, 1);
-  }
-
-  function onPointerDown(evt: PointerEvent) {
-    if (evt.isPrimary) {
-      playLeftMouseClickSound();
-    } else {
-      playRightMouseClickSound();
-    }
-  }
-
-  function onPointerUp(evt: PointerEvent) {
-
-  }
-
-  useEffect(() => {
-    document.addEventListener('keydown', onKeyboardPress);
-    document.addEventListener('pointerdown', onPointerDown);
-    document.addEventListener('pointerup', onPointerUp);
-
-    return () => {
-      document.removeEventListener('keydown', onKeyboardPress);
-      document.removeEventListener('pointerdown', onPointerDown);
-      document.removeEventListener('pointerup', onPointerUp);
-    }
-  }, []);
-
-  return <></>
-}
-*/
