0f8127caf9729ee1c556d7eb2a322d40764e5739
Joey de Ruiter <contact@joeyderuiter.me>
2024-02-13 15:25:07 +0100

    Added a title to the web version :ˆ)

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,7 +1,41 @@
-import { SceneLoader, Renderer } from "../components";
+import Head from "next/head";
+import { SceneLoader } from "../components";
+import { useEffect, useState } from "react";
+
+const focusedTitle = "Joey de Ruiter - Portfolio";
+const blurredTitle = "👀 Joey de Ruiter - Portfolio";
 
 export default function Web() {
+  const [title, setTitle] = useState("Joey de Ruiter - Portfolio");
+
+  function onVisibilityChange() {
+    const title = document.visibilityState === 'visible' ? focusedTitle : blurredTitle;
+
+    setTitle(title);
+  }
+
+  useEffect(() => {
+    document.addEventListener('visibilitychange', onVisibilityChange);
+
+    return () => {
+      document.removeEventListener('visibilitychange', onVisibilityChange);
+    }
+
+  }, []);
+
   return (
-    <SceneLoader />
+    <>
+      <Head>
+        <title>{title}</title>
+
+        <meta name="description" content="Portfolio website of Joey de Ruiter" />
+
+        <meta property="og:title" content="Joey de Ruiter - Portfolio" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/assets/thumbnail.png" />
+        <meta property="og:url" content="https://joeyderuiter.me/" />
+      </Head>
+      <SceneLoader />
+    </>
   );
 }
