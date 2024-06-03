c4a207c7eabe7184017bb393e3c9ba090d0ee1b6
Joey de Ruiter <contact@joeyderuiter.me>
2024-06-03 15:47:35 +0200

    wip: bubble sort viewer

--- apps/desktop/applications/AlgorithmVisualizer/BubbleSort/BubbleSort.tsx
+++ apps/desktop/applications/AlgorithmVisualizer/BubbleSort/BubbleSort.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from "react";
 import { SubViewParams } from "../AlgorithmVisualizerView";
-import { barGraph } from "@/components/GraphViewer/GraphViewer";
+import { BarGraph, barGraph } from "@/components/GraphViewer/GraphViewer";
 import { resizableWindowEventFilter } from "@/components/WindowManagement/WindowFilters";
 import { WindowEvent } from "@/components/WindowManagement/WindowEvents";
 
@@ -13,14 +13,18 @@ export default function BubbleSort(params: SubViewParams) {
   useEffect(() => {
     if (!graph.current) { return; }
 
+    const barGraph = new BarGraph();
+    if (!barGraph.bind(graph.current)) { return }
+
     const unsubscribe = compositor.subscribeWithFilter(
       windowId,
       resizableWindowEventFilter,
       (evt: WindowEvent) => {
         console.log(evt);
+
+        graphBar.resize(100, 100);
       });
 
-    barGraph(graph.current);
 
     return () => { unsubscribe() }
   }, []);
--- apps/desktop/components/GraphViewer/GraphViewer.ts
+++ apps/desktop/components/GraphViewer/GraphViewer.ts
@@ -1,5 +1,22 @@
-export function barGraph(element: HTMLCanvasElement) {
-  const context = element.getContext('2d');
 
-  console.log(context);
+interface Graph {
+  bind(element: HTMLCanvasElement): boolean;
+}
+
+export class BarGraph {
+  private context: CanvasRenderingContext2D | null = null;
+
+  constructor() {}
+
+  public bind(element: HTMLCanvasElement): boolean {
+    this.context = element.getContext('2d');
+
+    if (this.context === null) { return false; }
+
+    this.context.canvas.addEventListener('resize')
+
+    return true;
+
+    // return this.context !== null;
+  }
 }
