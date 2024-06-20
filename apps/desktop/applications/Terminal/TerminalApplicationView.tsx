4430b96ca9e1ffbca5933b8be412d21fbea73225
Joey de Ruiter <contact@joeyderuiter.me>
2024-06-20 15:04:38 +0200

    Load in xterm

--- apps/desktop/applications/Terminal/TerminalApplicationView.tsx
+++ apps/desktop/applications/Terminal/TerminalApplicationView.tsx
@@ -1,42 +1,29 @@
 import { WindowProps } from '@/components/WindowManagement/WindowCompositor';
-import { useEffect, useState } from 'react';
-
-function getTargetUrl(time: number): string {
-  const env = process.env.NEXT_PUBLIC_VERCEL_ENV ?? 'local';
-
-  if (env === 'local') {
-    return `http://192.168.178.134:8080/waifu?t=${time}`;
-  } else {
-    return `https://waifu.joeyderuiter.me/waifu?t=${time}`;
-  }
-}
+import { useEffect, useRef, useState } from 'react';
+import { Terminal } from '@xterm/xterm';
 
 export default function TerminalApplicationView(props: WindowProps) {
   const { application, windowContext } = props;
 
-  const [time, _] = useState(Date.now());
-  const url = getTargetUrl(time);
+  const terminalRef = useRef<HTMLDivElement>(null);
   
-  function onClickButton() {
-    application.apis.sound.play('/sounds/meow.mp3', 0.25);
-  }
-
   useEffect(() => { 
-    return () => { }
+    if (!terminalRef.current) { return; }
+
+    const terminal = new Terminal();
+    terminal.open(terminalRef.current);
+
+    return () => { 
+      
+    }
   }, []);
 
+  // I think loading a style sheet like this is illegal according to the HTML spec
+  // But the browsers accept it anyway :ˆ)
   return (
     <>
-      <button onClick={onClickButton}>click me</button>
-      <div style={{
-        width: '100%',
-        height: '100%',
-        display: 'block',
-        background: `url(${url})`,
-        backgroundSize: 'contain',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat'
-      }}></div>
+      <link rel="stylesheet" href="/xterm/xterm.css"/>
+      <div ref={terminalRef}></div>
     </>
   )
 } 
